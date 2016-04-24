4cb3805753017c1c3f9809b984b482acc4838820
Xiao Liu <liu.xiao@me.com>
2016-04-24 19:15:42 +0100

    Add new solution

---
+++ RemoveElement.java
@@ -0,0 +1,24 @@
+/**
+ * Problem: #27 Remove Element
+ *
+ * Idea: One point to count numbers of elements to delete, the other is for the current
+ * position to store the values that are not going to be deleted.
+ *
+ * Solutions:
+ *  removeElement: two pointers
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int counter = 0;
+        int current = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                counter ++;
+            } else {
+                nums[current] = nums[i];
+                current ++;
+            }
+        }
+        return nums.length - counter;
+    }
+}
