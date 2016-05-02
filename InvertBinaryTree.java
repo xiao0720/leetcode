e0ef4d41384c583e14349b13c34da5558e252982
Xiao Liu <liu.xiao@me.com>
2016-05-02 12:46:13 +0100

    Add new solution

---
+++ InvertBinaryTree.java
@@ -0,0 +1,55 @@
+/**
+ * Problem: 226. Invert Binary Tree
+ *
+ * Idea:
+ *
+ * Solutions:
+ *  invertTree: using invertTreeNode method, recursive calls, do the inversion until no
+ * children exists.
+ *
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTreeNode(TreeNode node) {
+        if (node.left != null) {
+            invertTreeNode(node.left);
+        }
+
+        if (node.right != null) {
+            invertTreeNode(node.right);
+        }
+
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        return node;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root != null) {
+            invertTreeNode(root);
+        }
+        return root;
+    }
+/*
+    public static void main(String[] args) {
+        InvertBinaryTree ibt = new InvertBinaryTree();
+        TreeNode tree = new TreeNode(4);
+        tree.left = new TreeNode(2);
+        tree.left.left = new TreeNode(1);
+        tree.left.right = new TreeNode(3);
+        tree.right = new TreeNode(7);
+        tree.right.left = new TreeNode(6);
+        tree.right.right = new TreeNode(9);
+        System.out.println(ibt.invertTree(tree));
+    }*/
+}
---
+++ TreeNode.java
@@ -0,0 +1,21 @@
+/**
+ * Class Used For Problem: 226. Invert Binary Tree
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        this.val = x;
+    }
+}
