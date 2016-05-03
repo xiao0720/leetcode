855eb9314a2facdc444d8f574131cb77ecddc054
Xiao Liu <liu.xiao@me.com>
2016-05-03 22:37:41 +0100

    Add new question file

---
+++ LargestNumber.java
@@ -0,0 +1,18 @@
+/**
+ * Problem: #179 Largest Number
+ *
+ * Ideas:
+ *
+ * Solutions:
+ *
+ *
+ */
+public class LargestNumber {
+    public String largestNumber(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LargestNumber().largestNumber(new int[] {3, 30, 34, 5, 9}));
+    }
+}
