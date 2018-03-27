34e830deb1955587e3b23c27fc2c6cc64caf0356
Xiao Liu <liu.xiao@me.com>
2018-03-27 17:38:04 +0100

    Update two sum

--- 1/Solution.py
+++ 1/Solution.py
@@ -1,17 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            try:
-                j = nums.index(target - nums[i])
-            except:
-                j = -1
-            if (j > -1) and (j != i): 
-                return [i, j] if i < j else [j, i]
+        lookup = {}
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return [lookup[target - num], i]
+            lookup[num] = i
 
 if __name__ == "__main__":
     print(Solution().twoSum([2, 2, 11, 15], 4))
