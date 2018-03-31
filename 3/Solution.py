6203715a15e47aa4ccba28f42f8db5ed0af2be45
Xiao Liu <liu.xiao@me.com>
2018-03-31 15:14:44 +0100

    Add initial working solution

---
+++ 3/Solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        used_chars = {}
+        current = 0
+        maximum = 0
+        for counter, value in enumerate(s):
+            if value not in used_chars:
+                used_chars[value] = counter
+                maximum = max(counter - current + 1, maximum)
+            else:
+                current = max(used_chars[value] + 1, current)
+                used_chars[value] = counter
+                maximum = max(counter - current + 1, maximum)
+        return maximum
+
+if __name__ == '__main__':
+    s = "tmmzuxt"
+    print(Solution().lengthOfLongestSubstring(s))
