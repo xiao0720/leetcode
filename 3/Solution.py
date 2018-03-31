8989e3d181377f3cdd5ad70c510fa4c2981df6ef
Xiao Liu <liu.xiao@me.com>
2018-03-31 16:49:00 +0100

    Clean up solution

--- 3/Solution.py
+++ 3/Solution.py
@@ -8,13 +8,10 @@ class Solution(object):
         current = 0
         maximum = 0
         for counter, value in enumerate(s):
-            if value not in used_chars:
-                used_chars[value] = counter
-                maximum = max(counter - current + 1, maximum)
-            else:
+            if value in used_chars:
                 current = max(used_chars[value] + 1, current)
-                used_chars[value] = counter
-                maximum = max(counter - current + 1, maximum)
+            used_chars[value] = counter
+            maximum = max(counter - current + 1, maximum)
         return maximum
 
 if __name__ == '__main__':
