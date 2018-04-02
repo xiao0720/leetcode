14666b0b2db40ec26ecfcb596a6c3de6481e7f72
Xiao Liu <liu.xiao@me.com>
2018-04-02 15:53:23 +0100

    Pending solution

--- 4/Solution.py
+++ 4/Solution.py
@@ -8,21 +8,21 @@ class Solution(object):
         counter = 0
         p = 0
         q = 0
-        
+
         m1 = -1
         m2 = -1
 
         len_sum = len(nums1) + len(nums2)
 
         if len_sum % 2 == 0:
-            n = len_sum/2 - 1
+            n = len_sum / 2 - 1
             m = n + 1
         else:
-            n = int(len_sum/2)
+            n = len_sum // 2
             m = None
 
         max = m if m else n
-        
+
         while counter <= max:
             counter += 1
 
@@ -43,7 +43,30 @@ class Solution(object):
                 m1, m2 = m2, nums2[q]
                 q += 1
 
-        return (m1 + m2)/2 if m else m2
+        return (m1 + m2) / 2 if m else m2
+
+
+# TODO: Add O(log(m, n)) solution
+class Solution1(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        m = len(nums1)
+        n = len(nums2)
+
+        p = int(m / 2)
+        q = int(n / 2)
+
+        middle = 0
+
+        # if nums1[p] <= nums2[q]:
+        #     nums1[p+1] .. nums1[m - 1]
+        #     nums2[0] .. nums2[q]
 
 
 if __name__ == '__main__':
+    nums1 = [1, 2]
+    nums2 = [3, 4, 5, 6, 7, 8, 9]
+    print(Solution().findMedianSortedArrays(nums1, nums2))
+
+    [2]
+
+    [3, 4, 5, 6]
