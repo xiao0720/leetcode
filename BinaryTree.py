2876f1c0f5db163430c30b60035fb6dc182e59da
Xiao Liu <liu.xiao@me.com>
2018-04-21 14:30:08 +0100

    Add Binary Tree

---
+++ BinaryTree.py
@@ -0,0 +1,24 @@
+from Tree import Tree
+
+class BinaryTree(Tree):
+    def left(self, p):
+        raise NotImplementedError('must be implemented by subclass')
+
+    def right(self, p):
+        raise NotImplementedError('must be implemented by subclass')
+
+    def sibling(self, p):
+        parent = self.parent(p)
+        if parent is None:
+            return None
+        else:
+            if self.left(parent) == p:
+                return self.right(parent)
+            else:
+                return self.left(parent)
+
+    def children(self, p):
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p)
